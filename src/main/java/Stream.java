16c6277a1a3ba2761a056bfe757b8e1b7af2f5db
KIKI0911 <n.daiki911@gmail.com>
2023-10-22 23:19:16 +0900

    List追加

--- src/main/java/Stream.java
+++ src/main/java/Stream.java
@@ -1,5 +1,8 @@
+import java.util.List;
+
 public class Stream {
     public static void main(String[] args){
-
+    List<String>list = List.of("aichi","kyoto","tokyo","osaka");
+    list.stream().map(String::toUpperCase).forEach(System.out::println);
     }
 }
